950ca996ee39fef481b84ab1da8722b5221a516f
florian <martinflorian2@gmail.com>
2021-06-25 13:23:52 +0200

    clean code, no changes

--- functions.js
+++ functions.js
@@ -1,7 +1,19 @@
 const functions = {
+    /**
+     * 
+     * @param {number} min 
+     * @param {number} max 
+     * @returns a number randomly between min and max
+     */
     getRandomNumber: (min, max) => {
         return Math.floor(Math.random(min + max) * max - min);
     },
+    /**
+     * 
+     * @param {number} min 
+     * @param {number} max 
+     * @returns a number half way between min and max
+     */
     makeGuess: (min, max) => {
         return Math.floor((max + min) / 2);
     }
